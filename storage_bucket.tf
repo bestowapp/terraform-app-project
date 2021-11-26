794b76da7ee864f239aaf3ee1eb205b2726038fe
Ryan Hanks <ryandhanks@gmail.com>
2021-11-26 17:17:10 -0600

    wip

--- storage_bucket.tf
+++ storage_bucket.tf
@@ -1,10 +1,4 @@
 
-resource "google_storage_bucket_iam_member" "member" {
-  bucket = google_storage_bucket.state.name
-  role   = "roles/storage.admin"
-  member = "serviceAccount:${module.app_project.service_account_email}"
-}
-
 resource "google_storage_bucket" "state" {
   name                        = "${module.app_project.project_id}-state"
   location                    = "US"
